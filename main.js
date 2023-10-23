acd6ff2be6294686acfad186ed5d37533c2ca614
Syagr <114944986+Syagr@users.noreply.github.com>
2023-10-23 18:05:31 +0300

    I've written the whole js file for the api to work

--- main.js
+++ main.js
@@ -1,57 +1,77 @@
 const apiKey = 'a502393cf614401c9e0165300231910';
 
-// http://api.weatherapi.com/v1/current.json?key=a502393cf614401c9e0165300231910&q=London
-
-
-
-
-
+// http://api.weatherapi.com/v1/current.json?key=a502393cf614401c9e0165300231910&q=London--------
 
 // Элементы на странице
 const header = document.querySelector('.header');
 const form = document.querySelector('#form');
 const input = document.querySelector('#inputCity');
 
+function removeCard() {
+  // Удаляем предыдущую карточку
+  const prevCard = document.querySelector('card');
+  if (prevCard) prevCard.remove();
+}
 
 //  слушаем отправку формы
 
-form.onsubmit = function (e) {
+function showError(errorMessage) {
+  // отображаем карточку с ошибкой
+  const html = `<div class="card">${errorMessage}</div>`;
+  // отображаем карточку на странице
+  header.insertAdjacentHTML('afterend', html);
+}
+
+function showCardError({ name, country, temp, condition }) {
+  const html = `<di class="card">
+	<h2 class="card-city">${name},<span>${country}</span></h2>
+
+	<div class="card-weather" >
+	<div class="card-value">${temp}<sup>°c</sup></div>
+	<img class ="card-img" src="./image/weather.png" alt="weather">
+	</div>
+
+	<div class="card-description">${condition}</div>
+	</div>`;
+
+  header.insertAdjancentHTML('afterend', html);
+}
+
+async function getWeather(city) {
+  // адрес запроса
+  const url =
+    'http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}';
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+form.onsubmit = async function (e) {
   // отменяем отпраку формы
   e.preventDefault();
 
   // берем значение из инпута, обрезаем пробелы
-   let city = input.value.trim();
-	// делаем запрос на сервер
-	// адрес запроса
-	const url = 'http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}';
-	
-	// выполняем запрос
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-		console.log(data);
-		console.log(data.location.name);
-		console.log(data.location.country);
-		console.log(data.current.temp_c);
-		console.log(data.current.condition.text);
+  let city = input.value.trim();
 
-		// отоброжаем данные в карточке
-		// разметка для карточки
-		const html = `
-		<div class="card">
-	<h2 class="card-city">${data.location.name} <span>${data.location.country}</span></h2>
-	<div class="card-weather">
-		<div class="card-value">
-			${data.current.temp_c}<sup>°c</sup>
-		</div>
-		<img class="card-img" src="./image/weather.png" alt="weather">
-	</div>
-	<div class="card-description">${data.current.condition.text}</div>
-</div>`
-		
-		// отображаем карточку на странице
-		header.insertAdjancentHTML('afterend', html);
-    });
-}
+  // получаем данные с сервера
+  const data = await getWeather(city);
+
+  if (data.error) {
+    removeCard();
+
+    showError(data.error.message);
+  } else {
+    removeCard();
+
+    const weatherData = {
+      name: data.location.name,
+      country: data.location.country,
+      temp_c: data.current.temp_c,
+      condition: data.current.condition.text,
+    };
+    showCard(weatherData);
+  }
+
+ 
+};
